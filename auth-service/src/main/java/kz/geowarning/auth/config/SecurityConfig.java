6555529558659079c3595e7c53d694db2cdb13fe
Sabina <s.yesniyazova@p-s.kz>
2023-09-24 02:12:57 +0600

    Set up discovery and auth, configured Swagger

---
+++ auth-service/src/main/java/kz/geowarning/auth/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package kz.geowarning.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll() // Allow all requests
+                .anyRequest().authenticated() // Authenticate all other requests
+                .and()
+                .csrf().disable(); // Disable CSRF protection (optional)
+    }
+}
