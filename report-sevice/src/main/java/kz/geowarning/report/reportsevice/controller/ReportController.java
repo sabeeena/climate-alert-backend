e253260fdf4c6964c6ed9a12195a19ef0c6f511e
Adilbaeva Meruert <adilbayevameruert@gmail.com>
2023-10-30 21:57:55 +0600

    report-service

---
+++ report-sevice/src/main/java/kz/geowarning/report/reportsevice/controller/ReportController.java
@@ -0,0 +1,38 @@
+package kz.geowarning.report.reportsevice.controller;
+
+import kz.geowarning.report.reportsevice.service.ReportService;
+import net.sf.jasperreports.engine.JRException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@RestController
+public class ReportController {
+    @Autowired
+    ReportService reportService;
+
+    @GetMapping("/fire-real-time-overall")
+    public ResponseEntity<?> generateAccessDenials(@RequestParam Long reportId,
+                                                   @RequestParam String lang, @RequestParam String type) throws JRException, SQLException, IOException, IllegalAccessException {
+        byte[] bytes = reportService.exportReportFireRealTimeOverall(reportId, lang);
+        ByteArrayResource resource = new ByteArrayResource(bytes);
+
+        if (bytes.length == 0) {
+            return ResponseEntity.status(204).build();
+        } else {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Report." + type)
+                    .body(resource);
+        }
+    }
+
+}
---
+++ report-sevice/src/test/java/kz/geowarning/report/reportsevice/ReportSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.geowarning.report.reportsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
