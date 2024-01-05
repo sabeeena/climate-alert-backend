94dfcdd2a4af17c80b3886fa5172536a0c34b281
Adilbaeva Meruert <adilbayevameruert@gmail.com>
2024-01-05 11:39:59 +0600

    with bugs

--- auth-service/src/main/java/kz/geowarning/auth/AuthApplication.java
+++ auth-service/src/main/java/kz/geowarning/auth/AuthApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -19,6 +21,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Slf4j
 @EnableAsync
+@EnableZuulProxy
+@EnableConfigurationProperties
 public class AuthApplication implements ApplicationRunner {
 
 	@Autowired
