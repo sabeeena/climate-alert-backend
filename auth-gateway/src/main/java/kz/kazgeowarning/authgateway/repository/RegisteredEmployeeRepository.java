d5a40832e54e6e1d03e4ca73902f0a6c7bb310e6
deadpearl <adilbayevameruert@gmail.com>
2024-02-23 18:41:18 +0600

    fixed get by employee

--- auth-gateway/src/main/java/kz/kazgeowarning/authgateway/repository/RegisteredEmployeeRepository.java
+++ auth-gateway/src/main/java/kz/kazgeowarning/authgateway/repository/RegisteredEmployeeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Set;
 
 public interface RegisteredEmployeeRepository extends JpaRepository<RegisteredEmployee, Long> {
-    @Query(nativeQuery = true, value = "select * from registered_employee where admin_id = " +
-            "(select admin_id from registered_employee where employee_email=:email)")
+    @Query(nativeQuery = true, value = "select * from auth.registered_employee where admin_id =\n" +
+            "            (select admin_id from auth.registered_employee where auth.registered_employee.employeeemail=:email)")
     Set<RegisteredEmployee> findAllEmployeeByEEmail(String email);
 }
