80c439088dbf47477ed62aa7814dcbc877e07923
Adilbayeva Meruyert <adilbayevameruert@gmail.com>
2025-12-09 10:40:54 +0500

    fix localhost

--- auth-service/src/main/resources/db/changelog/data/roles.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
---changeset Sabina:roles.sql
-INSERT INTO "role"(id, name, role_code) VALUES (nextval('role_seq'), 'Administrator', 'ADMIN');
-INSERT INTO "role"(id, name, role_code) VALUES (nextval('role_seq'), 'Authority', 'SUPERVISOR');
-INSERT INTO "role"(id, name, role_code) VALUES (nextval('role_seq'), 'Community', 'USER');
-
